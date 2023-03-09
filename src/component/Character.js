131fdcf3a8b6c67af2cc4e89aa744f5f547f65f7
paulpaulchan <116852901+ngwingyan@users.noreply.github.com>
2023-03-09 20:51:26 +0000

    add alt properties

--- src/component/Character.js
+++ src/component/Character.js
@@ -5,7 +5,7 @@ const Character = props => {
     return (
         <div data-test="component-character">
             <h1 data-test='char-name'>{name}</h1>
-            <img src={imgUrl} data-test='char-img'/>
+            <img src={imgUrl} alt={name} data-test='char-img'/>
             <ul data-test='char-list'>
                 <li data-test = 'char-birth'>Date of Birth: {birth}</li>
                 <li data-test = 'char-death'>Date of Death: {death}</li>
